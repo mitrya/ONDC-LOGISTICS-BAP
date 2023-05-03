fb2a6baa24854862a4dfb07638cbbf460769b86f
unknown <kalyanij967@gmail.com>
2023-05-03 16:20:11 +0530

    Changes in about,homepage

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -5,21 +5,23 @@ import useWindowDimensions from '../../hooks/useWindowDimensions';
 import './homepage.styles.css'
 const Homepage = () => {
     
-    const { height, width } = useWindowDimensions();
-    useEffect(() => {
-      document.title='LogiGo'
-        if(width>1050 && height>700)
-           { 
-             window.scrollTo(0, 0)
-             document.body.style.overflow = "hidden";
-           }
-        return () => (document.body.style.overflow = "scroll");
-    });
+    // const { height, width } = useWindowDimensions();
+    // useEffect(() => {
+    //   document.title='LogiGo'
+    //     if(width>1050 && height>700)
+    //        { 
+    //          window.scrollTo(0, 0)
+    //          document.body.style.overflow = "hidden";
+    //        }
+    //     return () => (document.body.style.overflow = "scroll");
+    // });
     
     
   return (
-            <div className="form-background">
-                    <MultiStepForm/>
+            <div className="form-background bg">
+              <div className="bgcontent">
+                <MultiStepForm/>
+              </div>
             </div>
   )
 }
