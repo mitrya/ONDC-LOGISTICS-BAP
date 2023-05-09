13161502007db449182d54daf6fc075bbe75c669
Priyamvada Priyadarshani <75827294+Priyamvada-iiita@users.noreply.github.com>
2023-05-10 01:06:11 +0530

    Update profile.component.jsx

--- client/src/pages/Dashboard/profile.component.jsx
+++ client/src/pages/Dashboard/profile.component.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import AddressForm from '../../components/addressForm/addressForm.component';
 import { Button } from 'react-bootstrap';
 import './profile.styles.css'
-
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
   const [user, setuser] = useState(JSON.parse(localStorage.getItem('user')));
@@ -14,25 +14,66 @@ const Profile = () => {
     setupAdd(!upAdd)
   }
   return (
-    <div className='mt-5 d-flex flex-column justify-content-center container'> 
-
-      <div className="row">
-        <span>Welcome ✨</span>
-        <h1>{user.name.toUpperCase()}</h1>
+    <div id="My-Account-super-container-fluid box d-flex">
+      <div className='heading-account' style={{margin:'20px',textAlign:'center' , fontWeight:'700', marginLeft:'10px', fontSize:'50px'}}>
+    <p> My Account</p>
       </div>
-
-      {/* <div className='container'> */}
-       {
+      <div className='Account-container'> 
+    
+    <div className='float-container'>
+    <img  id="img" src="cat.jpg" alt="Description of the image"></img>
+    <div id="user-account-data">
+        
+        <p>{user.name.toUpperCase()}</p>
+        <p>{user.email}</p>
+        <div className='address-account'>
+  {user.address ? (
+    <div>
+      <p>Address: Door:{user.address.door}, Street:{user.address.street}, Pincode: {user.area_code}</p>
+      <p>City-{user.address.city}, State-{user.address.state},  Country-{user.address.country} </p>
+    </div>
+  ) : (
+    <p>Address not provided</p>
+  )}
+</div>
+      {
         (upAdd) ? <div className="row"><AddressForm ></AddressForm> </div> : <></> 
        }
-       {/* {
-        (JSON.stringify(addr) === '{}') ?  */}
-        <div className="row">
-          <Button className={upAdd ? 'hidden myBtn' : 'myBtn'} onClick={handleClick}>{upAdd ? 'Cancel' : 'Update My Address'}</Button>
+       
+        <div className="row-add">
+          <Button id={upAdd ? 'hidden Update-address' : 'Update-address'} onClick={handleClick} style={{backgroundColor:'#577D86', width:'40%', marginLeft:'32%'}}>{upAdd ? 'Cancel' : 'Update My Address'}</Button>
         </div> 
-      {/* } */}
-       {/* </div> */}
+        
+  </div>
+  </div>
+
+  <div className='float-container'>
+    <div id="welcoming" style={{textAlign:'center'}}>
+      <h1>Welcome! ✨ {user.name.toUpperCase()}</h1>
+      <h5> Unify Your Logistics Experience with ONDC Consumer application:
+         Bringing Convenience to Your Doorstep</h5>
     </div>
+  <div className='cards-in-row' style={{alignSelf:'center'}}>
+    <div className='card-account' style={{backgroundColor:'#dbc3e4'}}onClick={() => window.location.href = '/'}>
+    <img  id="image-cards-account" src="/order-icon.jpg"  style={{display:'block', margin:'40px', marginTop:'40px',borderRadius:'50%', width:'150px', height:'150px', objectFit:'cover', objectPosition:'center'}}></img>
+        <p>Explore and Order</p>
+        <p>&#10140;</p>
+        </div>
+        <div className='card-account'style={{backgroundColor:'#d1e4c3'}}onClick={() => window.location.href = '/orders'}>
+        <img  id="image-cards-account" src="/parcel.jpg" style={{display:'block', margin:'40px', marginTop:'40px',borderRadius:'50%', width:'150px', height:'150px', objectFit:'cover', objectPosition:'center'}}></img>
+        <p>Your Orders</p>
+        <p> &#10140;</p>
+        </div>
+</div>
+  
+</div>
+      
+       
+    </div>
+      
+      </div>
+    
+    
   )
 }
 
