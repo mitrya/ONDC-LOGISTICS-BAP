358d15f261d06f86cce1b730a4d7ce50c3cb505a
Vineeth Kumar Munigyala <munigyala@gmail.com>
2023-05-11 12:49:53 +0530

    server cors update

--- server/src/app.js
+++ server/src/app.js
@@ -8,15 +8,11 @@ require('./models/person');
 require('./models/order')
 const mongoose = require('mongoose');
 const stripe =require('stripe');
-// const {MONGO_URI} = process.env;
-// const MONGO_URI = 'mongodb+srv://miniPROJ:AAKPV@2023@cluster0.jc8n4yz.mongodb.net/?retryWrites=true&w=majority'
 require('dotenv').config();
 
-//asdsa
 mongoose.connect(
 
 	process.env.MONGO_URI,{
-	// 'mongodb://127.0.0.1:27017/courier_app',{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	}
@@ -34,8 +30,8 @@ app.use(express.json())
 app.use(cors({
 	origin: '*',
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-	credentials: true
+	// allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+	// credentials: true
 }));
 	
 app.set('views','./views');    // specify a folder to look for the views.
